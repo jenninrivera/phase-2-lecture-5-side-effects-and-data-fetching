9270905b50d79548c47966bc6166024f7a001158
Jenni Rivera <jenninrivera@gmail.com>
2023-08-25 14:01:11 -0400

    lecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import Header from "./Header"
 import PetPage from "./PetPage"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 function App() {
 
@@ -9,6 +9,11 @@ function App() {
 
   const [pets, setPets] = useState([])
 
+  useEffect (() => {
+    fetch('http://localhost:4000/pets')
+      .then(response => response.json())
+      .then(petData => setPets(petData))
+}, [])
   const filteredPets = pets.filter(pet => {
     if(searchText === ""){
       return true
@@ -17,15 +22,33 @@ function App() {
   })
 
   function adoptPet(id){
-    setPets(pets.filter(pet => {
+    fetch(`http://localhost:4000/pets/${id}`, {
+      method: "DELETE"
+    })
+    .then(response => {
+      if(response.ok){
+        setPets(pets.filter(pet => {
       return pet.id !== id
     }))
+      }
+    })
+    
   }
 
   function addPet(event){
     event.preventDefault()
+    console.log(formData)
 
-    setPets([...pets, {id: pets.length + 1, ...formData}])
+    fetch('http://localhost:4000/pets', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(newPet => setPets([...pets, newPet]))
   }
 
   function updateFormData(event){
